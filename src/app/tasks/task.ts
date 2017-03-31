27e803ac47dd537de1afee9829acc56a1f5cc64f
Shohrukh Davlatov <shohrukh.92@gmail.com>
2017-03-31 14:02:09 +0300

    Task class code refactoring (constructor vars)

--- src/app/tasks/task.ts
+++ src/app/tasks/task.ts
@@ -5,18 +5,20 @@ export class Task {
   title: string;
   isCompleted: boolean;
   _id: string;
-  parentList: List;
+  listId: string;
+  listTitle: string;
 
-  constructor() {
-
-  }
+  /*constructor(newTitle: string = "", newIsCompleted: string = "", ) {
+    forE
+    { this.title, this.isCompleted, this._id, this.listId, this.listTitle } = taskObject;
+  }*/
 
   convertToUrlParams() {
     let urlSearchParams = new URLSearchParams();
     urlSearchParams.append('title', this.title);
     urlSearchParams.append('isCompleted', String(this.isCompleted));
     urlSearchParams.append('_id', this._id);
-    urlSearchParams.append('listId', this.parentList._id);
+    urlSearchParams.append('listId', this.listId);
     return urlSearchParams.toString();
   }
 }
