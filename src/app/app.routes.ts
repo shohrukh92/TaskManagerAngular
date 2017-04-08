1a02696116dd859f82ed063238214c39c28c7362
Shohrukh Davlatov <shohrukh.92@gmail.com>
2017-04-08 12:10:15 +0300

    Redirected other (**) routes to default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { AboutComponent } from './about/about.component';
 import { ListsComponent } from './lists/lists.component';
 import { TasksComponent } from './tasks/tasks.component';
 
-export const RootRouterConfig: Routes = [
-    { path: '', redirectTo: 'lists', pathMatch: 'full' },
+export const RootRouterConfig: Routes = [    
     { path: 'lists', component: ListsComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'tasks', component: TasksComponent }
+    { path: 'tasks', component: TasksComponent },
+    { path: '**', redirectTo: 'lists', pathMatch: 'full' }
 ];
 
