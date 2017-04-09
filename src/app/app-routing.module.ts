1a8bda8150c22c799ea2830db65554376ebbe081
Shohrukh Davlatov <shohrukh.92@gmail.com>
2017-04-09 13:34:45 +0300

    Created separate App Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { ListsComponent } from './lists/lists.component';
 import { TasksComponent } from './tasks/tasks.component';
 
-export const RootRouterConfig: Routes = [    
+const routes: Routes = [
     { path: 'lists', component: ListsComponent },
     { path: 'about', component: AboutComponent },
     { path: 'tasks', component: TasksComponent },
     { path: '**', redirectTo: 'lists', pathMatch: 'full' }
 ];
 
+@NgModule({
+  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RootRouterConfig } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
 import { ListsComponent, ListsTableComponent, AddListFormComponent } from './lists';
 import { TasksComponent, TasksTableComponent, AddTaskFormComponent } from './tasks';
@@ -30,7 +29,7 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
         ReactiveFormsModule,
         HttpModule, 
         JsonpModule,
-        RouterModule.forRoot(RootRouterConfig, { useHash: true })
+        AppRoutingModule
     ],
     providers: [
         ApiService,
