3ebc5bdff5e41d5b38f5a1bcc77556c2f490bca8
Shohrukh Davlatov <shohrukh.92@gmail.com>
2017-04-15 21:50:31 +0300

    Use getter and setter for Lists input in ListsTableComponent

--- src/app/lists/lists-table.component.ts
+++ src/app/lists/lists-table.component.ts
@@ -8,11 +8,20 @@ import { List } from './list';
 })
 
 export class ListsTableComponent {
-    @Input() lists: List[];
-    rowHighlightColor: string = '#d2f3c7';
+    private _lists: List[];
+    @Input()
+    set lists(lists: List[]) {
+        this._lists = lists;
+    }
+    get lists(): List[] {
+        return this._lists;
+    }
+
+    rowHighlightColor: string;
 
     constructor(private _listsApi: ListsApi) {
-        this.lists = [];
+        this._lists = []; //set default value for lists array
+        this.rowHighlightColor = '#d2f3c7'; //color for highlighting table rows (for directive)
     }
 
     deleteList(list: List) {
